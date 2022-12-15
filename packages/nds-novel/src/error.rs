c56b59b6d2ed46b921156a658daf6569924f9342
Nero <resqnetm@gmail.com>
2022-12-15 05:19:52 +0300

    Add thiserror crate to the workspace dependencies & implement Error for the JumpToLabelError enum

--- packages/nds-novel/src/error.rs
+++ packages/nds-novel/src/error.rs
@@ -1,4 +1,7 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+use thiserror::Error;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Error)]
 pub enum JumpToLabelError {
+    #[error("Specified label was not found")]
     LabelNotFound,
 }
